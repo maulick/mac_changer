bf0b09e53ffa345c6a8b51b85a12ed22b43b1622
Maulick Metkar <44560123+maulick@users.noreply.github.com>
2020-07-22 22:39:26 +0530

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,12 +1,17 @@
-#!/usr/bin/env python
-
 import subprocess
+import argparse
 
-interface = input("interface > ")
-new_mac = input("new MAC > ")
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
+    parser.add_argument("-m", "--mac", dest="new_mac", help="New MAC Address")
+    return parser.parse_args()
 
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+args = get_arguments()
+change_mac(args.interface, args.new_mac)

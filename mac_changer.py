8dd2288a724a5988e05f9f57945795980c4a74f6
Maulick Metkar <44560123+maulick@users.noreply.github.com>
2020-07-27 23:18:11 +0530

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -5,7 +5,12 @@ def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
     parser.add_argument("-m", "--mac", dest="new_mac", help="New MAC Address")
-    return parser.parse_args()
+    args = parser.parse_args()
+    if not args.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not args.new_mac:
+        parser.error("[-] Please specify a new mac, use --help for more info.")
+    return args
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)

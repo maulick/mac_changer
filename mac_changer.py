cf68d4d336fbc4ccda00ef580cd893879b684b06
Maulick Metkar <44560123+maulick@users.noreply.github.com>
2020-07-28 23:30:38 +0530

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,5 +1,6 @@
 import subprocess
 import argparse
+import re
 
 def get_arguments():
     parser = argparse.ArgumentParser()
@@ -17,6 +18,17 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
+    
+def get_currnet_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC Address.")
 
 args = get_arguments()
-change_mac(args.interface, args.new_mac)
+current_mac = get_current_mac(args.interface)
+print("Current MAC = " + str(current_mac))
+#change_mac(args.interface, args.new_mac)
